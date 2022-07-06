925071855ca24a53d926b930c21f2684f19cfcda
KavishkaGunathilaka <gihanthakavishka@gmail.com>
2022-07-06 19:43:13 +0530

    connected aure database

--- database/db_helper.js
+++ database/db_helper.js
@@ -1,21 +1,38 @@
 require('dotenv').config();
+const fs = require('fs');
 
 var mysql = require('mysql');
 
-var con = mysql.createConnection({
-  host: process.env.DB_HOST,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_DATABASE,
-  multipleStatements: true
-});
-
 // var con = mysql.createConnection({
-//   host: 'localhost',
-//   user: 'root',
-//   password: 'password',
-//   database: 'hrms',
+//   host: process.env.DB_HOST,
+//   user: process.env.DB_USER,
+//   password: process.env.DB_PASSWORD,
+//   database: process.env.DB_DATABASE,
 //   multipleStatements: true
 // });
+
+var config =
+{
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_DATABASE,
+    port: 3306,
+    ssl: {ca: fs.readFileSync(__dirname +'/DigiCertGlobalRootCA.crt.pem')}
+};
+
+const con = new mysql.createConnection(config);
+
+con.connect(
+    function (err) { 
+    if (err) { 
+        console.log("!!! Cannot connect !!! Error:");
+        throw err;
+    }
+    else
+    {
+       console.log("Connection established.");
+    }
+});
 
 module.exports = con;
